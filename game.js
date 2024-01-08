609b8c69791f2f66b49ab05e7e18e70607aeb7b9
mugunthan s <mugunthan701@gmail.com>
2024-01-08 16:02:18 +0530

    comp

--- game.js
+++ game.js
@@ -1,19 +1,72 @@
-// Iteration 1: Declare variables required for this game
+const game_body = document.getElementById("game-body");
+let shotgunSound = new Audio("./assets/shotgun.wav");
 
-// Iteration 1.2: Add shotgun sound
+game_body.onclick = () => {
+    shotgunSound.currentTime = 0;
+    shotgunSound.play();
+};
 
-// Iteration 1.3: Add background sound
+let bgm = new Audio("./assets/bgm.mp3");
+bgm.play();
+bgm.loop = true;
+let zombie;
+let zombieID = 0;
 
-// Iteration 1.4: Add lives
+function generateZombies() {
+    let num = generateUniqueNums(1, 7);
+    game_body.innerHTML += `<img src=./assets/zombie-${num}.png  class=zombie-image id=Zombies${zombieID} >`;
+    zombie = document.getElementById(`Zombies${zombieID}`);
+    let second = generateUniqueNums(2, 7);
+    zombie.style.animationDuration = `${second}s`;
+    let viewwidth = generateUniqueNums(20, 80);
+    zombie.style.transform = `translateX(${viewwidth}vw)`;
 
-// Iteration 2: Write a function to make a zombie
+    zombie.onclick = () => {
+        destroyZombie(zombie);
+    };
+}
 
-// Iteration 3: Write a function to check if the player missed a zombie
+generateZombies();
 
-// Iteration 4: Write a function to destroy a zombie when it is shot or missed
+function generateUniqueNums(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
-// Iteration 5: Creating timer
+function destroyZombie(ghost) {
+    ghost.style.display = "none";
+    zombieID++;
+    generateZombies();
+}
 
-// Iteration 6: Write a code to start the game by calling the first zombie
 
-// Iteration 7: Write the helper function to get random integer
+
+let maxlife = document.getElementById("max-lives");
+let lives = 5; 
+let time = 60;
+
+function Escapezombie(zombie) {
+    let widthLife = 100;
+    if (zombie.getBoundingClientRect().top <= 0) {
+        widthLife -= 20;
+        lives--;
+       maxlife.style.width = `${widthLife}%`;
+        if (lives <= 0) {
+            location.href = "game-over.html";
+        } else {
+            destroyZombie(zombie);
+        }
+    }
+}
+
+
+setInterval(timer, 1000);
+
+function timer() {
+    if (time <= 0) {
+        location.href = "win.html";
+    } else {
+        time--;
+        document.getElementById("timer").innerText = time;
+        Escapezombie(zombie);
+    }
+}
